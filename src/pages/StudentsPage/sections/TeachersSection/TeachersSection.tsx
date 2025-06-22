eaa1fc37de4fe5acf15391a1f5e649428d93c0b9
sparkkkd <roydog228@gmail.com>
2025-06-22 13:36:35 +0300

    Final teacher section

--- src/pages/StudentsPage/sections/TeachersSection/TeachersSection.tsx
+++ src/pages/StudentsPage/sections/TeachersSection/TeachersSection.tsx
@@ -4,6 +4,7 @@ import clsx from 'clsx'
 import { TeachersSlider } from './components/TeachersSlider/TeachersSlider'
 
 import styles from './TeachersSection.module.sass'
+import { Container } from '../../../../components/Container/Container'
 
 interface TeachersSectionProps {
 	className?: string
@@ -12,10 +13,13 @@ interface TeachersSectionProps {
 export const TeachersSection: FC<TeachersSectionProps> = ({ className }) => {
 	return (
 		<section className={clsx(styles.teachers, className)}>
-			<h3 className={styles.title}>«Вот бы так в школе объясняли...»</h3>
-			<span className={styles.subtitle}>
-				Наши учителя так сильно любят свои предметы, что заражают этим учеников
-			</span>
+			<Container>
+				<h3 className={styles.title}>«Вот бы так в школе объясняли...»</h3>
+				<span className={styles.subtitle}>
+					Наши учителя так сильно любят свои предметы, что заражают этим
+					учеников
+				</span>
+			</Container>
 			<TeachersSlider />
 		</section>
 	)
--- src/pages/StudentsPage/sections/TeachersSection/components/TeachersSlider/TeachersSlider.tsx
+++ src/pages/StudentsPage/sections/TeachersSection/components/TeachersSlider/TeachersSlider.tsx
@@ -1,122 +1,32 @@
-import type { FC } from 'react'
+import { useRef, type FC } from 'react'
 import clsx from 'clsx'
-
+import { motion } from 'framer-motion'
 import { Splide, SplideSlide } from '@splidejs/react-splide'
 import '@splidejs/splide/dist/css/themes/splide-default.min.css'
 
-import teacher1 from '../../../../../../assets/teachers/teacher-1.jpg'
-import teacher2 from '../../../../../../assets/teachers/teacher-2.jpg'
-import teacher3 from '../../../../../../assets/teachers/teacher-3.jpg'
-import teacher4 from '../../../../../../assets/teachers/teacher-4.jpg'
-import teacher5 from '../../../../../../assets/teachers/teacher-5.jpg'
+import { TeacherCard } from '../TeacherCard/TeacherCard'
+
+import { TEACHERS } from './constant'
 
 import styles from './TeachersSlider.module.sass'
 import './splideOverwrite.sass'
-
-import { TeacherCard } from '../TeacherCard/TeacherCard'
+import { useWindowSize } from 'usehooks-ts'
 
 interface TeachersSliderProps {
 	className?: string
 }
 
-const TEACHERS = [
-	{
-		img: teacher1,
-		name: (
-			<>
-				Арман <br /> Костанян
-			</>
-		),
-		tags: ['Математика', '7 лет опыта'],
-		description:
-			'Директор ГАОУ «Лицей Иннополис». Победитель регионального этапа и призёр Всероссийского конкурса «Учитель года России» 2023',
-		info: [
-			{
-				count: '83',
-				text: 'средний балл ЕГЭ по математике',
-			},
-			{
-				count: '4,9',
-				text: 'средний балл ОГЭ по математике',
-			},
-		],
-	},
-	{
-		img: teacher2,
-		name: (
-			<>
-				Елизавета <br /> Романова
-			</>
-		),
-		tags: ['Химия', '7 лет опыта'],
-		description: 'Учитель, автор курсов по подготовке к ОГЭ и ЕГЭ по химии',
-		info: [
-			{
-				count: '86,4',
-				text: 'средний балл ЕГЭ у учеников',
-			},
-			{
-				count: '1 000+',
-				text: 'учеников подготовлено к ЕГЭ',
-			},
-		],
-	},
-	{
-		img: teacher3,
-		name: (
-			<>
-				Даниил <br /> Гурнович
-			</>
-		),
-		tags: ['Биология', '11 лет опыта'],
-		description: 'Репетитор по биологии. Преподаватель довуза Казанского ГМУ',
-		info: [
-			{
-				count: '98',
-				text: 'максимальный балл ЕГЭ по биологии',
-			},
-			{
-				count: '250',
-				text: 'учеников подготовлено к ЕГЭ и ОГЭ',
-			},
-		],
-	},
-	{
-		img: teacher4,
-		name: (
-			<>
-				Динар <br /> Габидуллин
-			</>
-		),
-		subject: 'Управление',
-		exp: '11 лет опыта',
-		tags: ['Управление', '11 лет опыта'],
-		description:
-			'Руководитель продакшн в kursnova. Основатель частного детского сада, соавтор школьных концепций. Создатель одной из ведущих инженерно-технических школ РоссииСУНЦ КНИТУ-КАИ',
-		info: [
-			{
-				count: '13',
-				text: 'лет в образовании — от учителя до директора',
-			},
-		],
-	},
-	{
-		img: teacher5,
-		name: <>Максим Булатов</>,
-		description:
-			'Основатель kursnova и офлайн-школы Skillbox в Казани. Сооснователь популярных YouTube-каналов («Секреты», «Космос Просто»)',
-		tags: ['Управление', '15+ лет опыта'],
-		info: [],
-	},
-]
-
 export const TeachersSlider: FC<TeachersSliderProps> = ({ className }) => {
+	const splideRef = useRef<any>(null)
+
+	const { width } = useWindowSize()
+
 	const options = {
 		// type: 'loop',
 		perPage: 4,
 		gap: 0,
-		arrows: true,
-		pagination: true,
+		arrows: false,
+		pagination: width < 815 ? true : false,
 		breakpoints: {
 			1068: {
 				perPage: 3,
@@ -130,7 +40,7 @@ export const TeachersSlider: FC<TeachersSliderProps> = ({ className }) => {
 
 	return (
 		<div className={clsx(styles.wrapper, className)}>
-			<Splide options={options} hasTrack={false}>
+			<Splide options={options} hasTrack={false} ref={splideRef}>
 				<div className='splide__track'>
 					<ul className='splide__list'>
 						{TEACHERS.map((props, index) => (
@@ -141,6 +51,22 @@ export const TeachersSlider: FC<TeachersSliderProps> = ({ className }) => {
 					</ul>
 				</div>
 			</Splide>
+			{width > 815 && (
+				<div className={styles.controls}>
+					<motion.button
+						className={styles.prev}
+						onClick={() => splideRef.current.go('<')}
+					>
+						Назад
+					</motion.button>
+					<motion.button
+						className={styles.next}
+						onClick={() => splideRef.current.go('>')}
+					>
+						Вперед
+					</motion.button>
+				</div>
+			)}
 		</div>
 	)
 }
